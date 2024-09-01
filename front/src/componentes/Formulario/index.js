9fe83a337923b94af13c094cdf8151435887e0da
PabloJ4 <008pablo008@gmail.com>
2024-09-01 14:22:37 -0300

    aleteração no formliario

--- front/src/componentes/Formulario/index.js
+++ front/src/componentes/Formulario/index.js
@@ -28,14 +28,14 @@ const Formulario = ({aoCadastrar, times}) => {
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
                 <CampoTexto
                     obrigatorio={true}
-                    label='Nome'
-                    placeholder='Digite seu nome '
+                    label='Empresa'
+                    placeholder='Digite o nome da sua empresa '
                     valor={nome}
                     aoAlterado={valor => setNome(valor)}/>
                 <CampoTexto
                     obrigatorio={true}
-                    label='Cargo' 
-                    placeholder='Digite seu cargo '
+                    label='Equipamento' 
+                    placeholder='Tipo de equipamento '
                     valor={cargo}
                     aoAlterado={valor => setCargo(valor)}/>
                 <CampoTexto 
@@ -44,11 +44,11 @@ const Formulario = ({aoCadastrar, times}) => {
                     aoAlterado={valor => setImagem(valor)}/>
                 <ListaSuspensa 
                     obrigatorio={true}
-                    label='Times'
+                    label='Situação do equipamento:'
                     items={times} 
                     valor={time}
                     aoAlterado={valor => setTime(valor)}/>
-                <Botao texto='Criar card' />
+                <Botao texto='Enviar' />
             </form>
         </section>
     )
