20ae605238006cc1d640db5f516c3dfa43302596
ricardobr3no <kbreeno.18@gmail.com>
2024-08-31 23:39:06 -0300

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import { connectToDatabase } from "./config/dbConnection.js";
+import routes from "./routes/index.js";
 
 const connection = await connectToDatabase();
 
-connection.on("error", (error)=>{
-    console.log("connection error")
+connection.on("error", (error) => {
+  console.log("connection error");
 });
 
-connection.once("open", ()=>{
-    console.log("Connection success.")
+connection.once("open", () => {
+  console.log("Connection success.");
 });
+
+routes(app);
 
 export const app = express();
