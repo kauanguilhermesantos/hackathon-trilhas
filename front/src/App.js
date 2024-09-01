e7b59b926db42bd77e033e3a12b88af214c37437
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-09-01 13:23:38 -0300

    update

--- front/src/App.js
+++ front/src/App.js
@@ -10,13 +10,13 @@ import Mapa from './componentes/Mapa';
 
 function App() {
     const times = [
-        { nome: 'Programação', corPrimaria: '#D9F7E9', corSecundaria: '#57C278' },
-        { nome: 'Front-End', corPrimaria: '#E8F8FF', corSecundaria: '#82CFFA' },
-        { nome: 'Data Science', corPrimaria: '#F0F8E2', corSecundaria: '#A6D157' },
-        { nome: 'Devops', corPrimaria: '#FDE7E8', corSecundaria: '#E06B69' },
+        { nome: 'conservado', corPrimaria: '#D9F7E9', corSecundaria: '#57C278' },
+        { nome: 'recuperavel - apresenta defeito', corPrimaria: '#E8F8FF', corSecundaria: '#82CFFA' },
+        { nome: 'nao recuperavel', corPrimaria: '#F0F8E2', corSecundaria: '#A6D157' },
+        /*{ nome: 'Devops', corPrimaria: '#FDE7E8', corSecundaria: '#E06B69' },
         { nome: 'UX e Design', corPrimaria: '#FAE9F5', corSecundaria: '#DB6EBF' },
         { nome: 'Mobile', corPrimaria: '#FFF5D9', corSecundaria: '#FFBA05' },
-        { nome: 'Inovação e Gestão', corPrimaria: '#FFEEDF', corSecundaria: '#FF8A29' },
+        { nome: 'Inovação e Gestão', corPrimaria: '#FFEEDF', corSecundaria: '#FF8A29' },*/
     ];
  // um card incial
     const inicial = [
--- front/src/componentes/Banner/index.js
+++ front/src/componentes/Banner/index.js
@@ -1,9 +1,11 @@
 import './banner.css'
+import imagemBanner from '../../assets/bannerimagem.png'
 
 const Banner = () => {
-    return (<header className="cabecalho">
-        <img src='/imagens/banner.png' alt='Logo do Organo'/>
-    </header>)
+    return (
+            <header className="cabecalho">
+                <img src={imagemBanner} alt={'Logo do Organo'}/>
+            </header>)
 }
 
 export default Banner
